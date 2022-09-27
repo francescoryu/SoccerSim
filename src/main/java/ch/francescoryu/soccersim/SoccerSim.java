96ac6504df13d918045ffd8ed64fb4ce84fcc6d6
francescoryu <francescoryu04@gmail.com>
2022-09-27 21:22:39 +0200

    added some stuff

--- src/main/java/ch/francescoryu/soccersim/SoccerSim.java
+++ src/main/java/ch/francescoryu/soccersim/SoccerSim.java
@@ -2,12 +2,18 @@ package ch.francescoryu.soccersim;
 
 import ch.francescoryu.soccersim.methods.MenuMethods;
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.shape.Box;
 import javafx.stage.Stage;
+
+import java.util.Collection;
+import java.util.Timer;
 
 /**
  * @author Francesco Ryu
@@ -18,7 +24,7 @@ import javafx.stage.Stage;
 public class SoccerSim extends Application {
     MenuMethods menuMethods = new MenuMethods();
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws InterruptedException {
         BorderPane borderPane = new BorderPane();
         borderPane.setStyle("-fx-background-color: linear-gradient(to bottom, #ffaf3d, #7b00ff);");
 
@@ -40,17 +46,65 @@ public class SoccerSim extends Application {
         welcomeLabelBox.getChildren().addAll(welcomeLabel);
         borderPane.setTop(welcomeLabelBox);
 
-        VBox team1Box = new VBox();
+        Label team1Label = new Label("Team 1");
+        team1Label.setStyle("-fx-font-family: 'Times New Roman'; -fx-font-size: 30");
 
-        HBox centerBox = new HBox();
-        centerBox.setStyle("-fx-border-color: black; -fx-border-insets: 10");
-        centerBox.getChildren().add(team1Box);
-        borderPane.setCenter(centerBox);
+        TextField team1TextField = new TextField();
+        team1TextField.setStyle("-fx-font-size: 15");
+        team1TextField.setPrefColumnCount(15);
+
+        Button clearButton = new Button("Clear");
+        menuMethods.setButtonStyle(clearButton);
+
+        VBox team1Box = new VBox();
+        team1Box.setSpacing(15);
+        team1Box.setAlignment(Pos.TOP_CENTER);
+        team1Box.setStyle("-fx-border-color: black; -fx-padding: 10");
+        team1Box.getChildren().addAll(team1Label, team1TextField, clearButton);
+
+        Label team2Label = new Label("Team 2");
+        team2Label.setStyle("-fx-font-family: 'Times New Roman'; -fx-font-size: 30");
+
+        TextField team2TextField = new TextField();
+        team2TextField.setStyle("-fx-font-size: 15");
+        team2TextField.setPrefColumnCount(15);
+
+        Button startButton = new Button("Start");
+        menuMethods.setButtonStyle(startButton);
+
+        VBox team2Box = new VBox();
+        team2Box.setSpacing(15);
+        team2Box.setAlignment(Pos.TOP_CENTER);
+        team2Box.setStyle("-fx-border-color: black; -fx-padding: 10");
+        team2Box.getChildren().addAll(team2Label, team2TextField, startButton);
+
+        clearButton.setOnAction(actionEvent -> {
+            menuMethods.deleteInputFromTextField(team1TextField, team2TextField);
+        });
+
+        //------------------------------------------------------------------------------------------------------------
+
+
+
+        //------------------------------------------------------------------------------------------------------------
+
+        BorderPane centerPane = new BorderPane();
+        centerPane.setStyle("-fx-border-color: black; " +
+                "-fx-border-insets: 10; " +
+                "-fx-padding: 10");
+        centerPane.setLeft(team1Box);
+        centerPane.setRight(team2Box);
+        borderPane.setCenter(centerPane);
 
         Scene scene = new Scene(borderPane, 700, 500);
         stage.setTitle("SoccerSim");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
+
+        startButton.setOnAction(actionEvent ->  {
+            centerPane.setVisible(false);
+
+        });
     }
 }
--- src/main/java/ch/francescoryu/soccersim/methods/MenuMethods.java
+++ src/main/java/ch/francescoryu/soccersim/methods/MenuMethods.java
@@ -1,6 +1,8 @@
 package ch.francescoryu.soccersim.methods;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class MenuMethods {
     public void setButtonStyle(Button button) {
@@ -12,5 +14,22 @@ public class MenuMethods {
                 "-fx-background-radius: 10;" +
                 "-fx-border-color: black;" +
                 "-fx-border-radius: 10");
+    }
+
+    public void deleteInputFromTextField(TextField textField1, TextField textField2) {
+        textField1.setText("");
+        textField2.setText("");
+    }
+
+    public void gameTimer(Label label) {
+        Thread thread = new Thread();
+        for (int i = 0; i < 91; i++) {
+            try {
+                thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            label.setText(i + " Mins played");
+        }
     }
 }
